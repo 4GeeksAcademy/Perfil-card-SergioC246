001c4e9d63f6df762b68d0d930d405d3dda82bd1
Sergio Cordoba <sergiocordoba246@gmail.com>
2025-11-10 11:18:03 +0000

    Entrega final

--- src/js/app.js
+++ src/js/app.js
@@ -8,12 +8,12 @@ import "../style/index.css";
         includeCover: true, // if includeCover is true the algorithm should show the cover image
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
         avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "right", // social media bar position (left or right)
-        
+        socialMediaPosition: "right", // social media bar position (left or right)   
+
         twitter: null, // social media usernames
         github: null,
         linkedin: null,
-        instagram: null,
+        instagram: null,    
 
         name: null,
         lastName: null,
@@ -33,14 +33,26 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${variables.name || "Sergio"} ${
+    variables.lastName ? variables.lastName : "Córdoba"
+  }</h1>
+          <h2>${
+            variables.role ? variables.role : "Desarrollador Web Junior"
+          } </h2>
+          <h3>${variables.city || "Madrid"} , ${
+    variables.country ? variables.country : "Spain"
+  }</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${
+              variables.twitter ? variables.twitter : "4geeksacademy"
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github ||
+              "4geeksacademy"}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin ? variables.linkedin : "school/4geeksacademy"
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram ||
+              "4geeksacademy"}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
